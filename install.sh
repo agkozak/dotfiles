f54b603127b7614fd86651072ba809d9fdcce9fa
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-11-18 10:35:25 -0800

    Delete .zlogin

--- install.sh
+++ install.sh
@@ -215,6 +215,7 @@ rm -f "$HOME/.zlogin" "$HOME/.zlogin.zwc" "$HOME/.zlogout" "$HOME/zlogout.zwc"
 # Clean up outdated files {{{1
 
 [ -f "${HOME}/.zprofile" ] && rm "${HOME}/.zprofile"
+[ -f "${HOME}/.zlogin" ] && rm "${HOME}/.zlogin"
 
 # Return to original directory {{{1
 
